41ae109e5a47d55a3b8790ccb3962906ceb96973
Shivay Bhandari <bhandarishivay2001@gmail.com>
2022-12-07 11:50:31 +0530

    react routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import { Route, Switch } from "react-router-dom";
+
+import AllMeetups from "./pages/AllMeetups";
+import NewMeetup from "./pages/NewMeetup";
+import Favorites from "./pages/Favorites";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Switch>
+        <Route path="/" exact>
+          <AllMeetups />
+        </Route>
+
+        <Route path="/new-meetup">
+          <NewMeetup />
+        </Route>
+
+        <Route path="/favorites">
+          <Favorites />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
