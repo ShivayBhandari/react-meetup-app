953def551ff40347300f2162e6f2c05d7132f8ed
Shivay Bhandari <bhandarishivay2001@gmail.com>
2022-12-07 16:02:57 +0530

    sending a POST HTTP request for form data to firebase server and database

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -3,10 +3,23 @@ import React from "react";
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 const NewMeetup = () => {
+  function addMeetupHandler(meetupData) {
+    fetch(
+      "https://react-meetup-app-caff7-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headrer: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  }
+
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
   );
 };
